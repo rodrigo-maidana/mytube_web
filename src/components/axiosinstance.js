e5d1264c3e74f59e5eca1827b7695ab332e9d50d
jorgef29 <jorgefigueredo2001@gmail.com>
2024-10-14 17:26:41 -0300

    axios instance

---
+++ src/components/axiosinstance.js
@@ -0,0 +1,23 @@
+// src/axiosInstance.js
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://modulo-compras.rodrigomaidana.com:8080/',
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  if (!config.url.includes('/auth/')) {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+      //console.log('Token incluido en la solicitud:', token); // Debugging token inclusion
+    } else {
+      console.log('No se encontró token en localStorage'); // Debugging missing token
+    }
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+export default axiosInstance;
