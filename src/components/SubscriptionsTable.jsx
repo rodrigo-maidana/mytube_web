aecfd770f27b7dcf78a07adc85b33bef79ae8ed8
jorgef29 <jorgefigueredo2001@gmail.com>
2024-11-07 13:51:21 -0300

    fix: SubscriptionsTables

--- src/components/SubscriptionsTable.jsx
+++ src/components/SubscriptionsTable.jsx
@@ -1,10 +1,10 @@
-// src/components/SubscriptionsTable.jsx
 import React, { useEffect, useState } from "react";
 import { Table, Spinner, Alert } from "react-bootstrap";
 import axiosInstance from "./axiosinstance";
 
 function SubscriptionsTable() {
   const [subscriptions, setSubscriptions] = useState([]);
+  const [usernames, setUsernames] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -12,8 +12,40 @@ function SubscriptionsTable() {
     // Obtener suscripciones
     const fetchSubscriptions = async () => {
       try {
-        const response = await axiosInstance.get("/subscriptions/all");
+        const response = await axiosInstance.get(
+          "http://mytube.rodrigomaidana.com:8081/subscriptions/all"
+        );
         setSubscriptions(response.data);
+
+        // Para cada suscripción, obten el nombre de usuario
+        const usernamePromises = response.data.map(async (subscription) => {
+          try {
+            const userResponse = await axiosInstance.get(
+              `http://mytube.rodrigomaidana.com:8081/users/${subscription.userId}`
+            );
+            return {
+              userId: subscription.userId,
+              username: userResponse.data.username,
+            };
+          } catch (error) {
+            console.error(
+              `Error al obtener el username para userId ${subscription.userId}:`,
+              error
+            );
+            return { userId: subscription.userId, username: "Desconocido" };
+          }
+        });
+
+        // Espera todas las promesas de nombres de usuario y actualiza el estado
+        const usernamesArray = await Promise.all(usernamePromises);
+        const usernamesMap = usernamesArray.reduce(
+          (acc, { userId, username }) => {
+            acc[userId] = username;
+            return acc;
+          },
+          {}
+        );
+        setUsernames(usernamesMap);
       } catch (error) {
         console.error("Error al obtener suscripciones:", error);
         setError("Error al obtener las suscripciones");
@@ -26,42 +58,49 @@ function SubscriptionsTable() {
   }, []);
 
   return (
-      <div className="container mt-4">
-        <h2 className="text-center mb-4">Suscripciones</h2>
+    <div className="container mt-4">
+      <h2 className="text-center mb-4">Suscripciones</h2>
 
-        {loading ? (
-            <div className="text-center">
-              <Spinner animation="border" role="status">
-                <span className="visually-hidden">Cargando...</span>
-              </Spinner>
-            </div>
-        ) : error ? (
-            <Alert variant="danger" className="text-center">
-              {error}
-            </Alert>
-        ) : (
-            <Table striped bordered hover responsive>
-              <thead>
-              <tr>
-                <th>User ID</th>
-                <th>Channel ID</th>
-                <th>Fecha de Suscripción</th>
+      {loading ? (
+        <div className="text-center">
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Cargando...</span>
+          </Spinner>
+        </div>
+      ) : error ? (
+        <Alert variant="danger" className="text-center">
+          {error}
+        </Alert>
+      ) : (
+        <Table striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th>Username</th>
+              <th>Channel ID</th>
+              <th>Fecha de Suscripción</th>
+            </tr>
+          </thead>
+          <tbody>
+            {subscriptions.map((subscription, index) => (
+              <tr key={index}>
+                <td>{usernames[subscription.userId] || "Cargando..."}</td>
+                <td>{subscription.channelId}</td>
+                <td>
+                  {new Date(subscription.subscriptionDate).toLocaleDateString(
+                    "es-ES",
+                    {
+                      day: "2-digit",
+                      month: "2-digit",
+                      year: "numeric",
+                    }
+                  )}
+                </td>
               </tr>
-              </thead>
-              <tbody>
-              {subscriptions.map((subscription, index) => (
-                  <tr key={index}>
-                    <td>{subscription.userId}</td>
-                    <td>{subscription.channelId}</td>
-                    <td>
-                      {new Date(subscription.subscriptionDate).toLocaleDateString()}
-                    </td>
-                  </tr>
-              ))}
-              </tbody>
-            </Table>
-        )}
-      </div>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </div>
   );
 }
 
